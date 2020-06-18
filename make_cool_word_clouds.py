efe6fe68c2f85f73ae51bb57984dbcf1e910d245
Robin Reicher <robin.o.reicher@gmail.com>
2020-06-18 15:13:59 +0200

    made some proper word-clouds in letter shapes

--- make_cool_word_clouds.py
+++ make_cool_word_clouds.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 from os import path
 from PIL import Image
+import random
+import re
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
@@ -13,88 +15,105 @@ import sys
 sys.path.append('RiksdagenPythonAPI/')
 import riksdagen
 
+def transform_format(val):
+    if val == 0:
+        return 255
+    else:
+        return val
 
-def do_word_cloud(text: Dict[riksdagen.Parti, str]):
+def do_cloud(text: str, parti: riksdagen.Parti):
 
-    clouds = []
-    fig = plt.figure(figsize=(4, 2))
+    mask_filename = f'{parti.name}.png'
+    mask = np.array(Image.open(f'mask/{mask_filename}'))
+    # Transform your mask into a new one that will work with the function:
+    transformed_mask = np.ndarray((mask.shape[0], mask.shape[1]), np.int32)
 
-    # ax enables access to manipulate each of subplots
-    ax = []
-    columns = 4
-    rows = 2
-    for p in riksdagen.Parti:
-        ax.append(fig.add_subplot(rows, columns, p.value + 1))
-        word_cloud = WordCloud(max_font_size=40).generate(text[p])
-        plt.imshow(word_cloud,  interpolation="bilinear")
-        plt.axis("off")
+    for i in range(len(mask)):
+        transformed_mask[i] = list(map(transform_format, mask[i]))
 
+    wordcloud = WordCloud(max_font_size=30, background_color="white", mask=transformed_mask).generate(text)
+    plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
     plt.show()
 
+    filename = f'{parti.name}_wordcloud.png'
+    wordcloud.to_file(f'pics/{filename}')
 
-def get_party_words(api, parti: riksdagen.Parti):
 
-    anforande_lista = api.get_anforande(rm='2019/20', parti=parti.name, anftyp='Nej')
+def get_party_words(api, parti: riksdagen.Parti, size: int):
+
+    anforande_lista = api.get_anforande(parti=parti.name, anftyp='Nej', antal=size)
+    print(f'Anföranden {parti.name}: {len(anforande_lista)}')
 
     word_sum = ""
     for anförande in anforande_lista:
-        word_sum += anförande.avsnittsrubrik + ' '  # full uncleaned string
+        clean = re.sub("[!#?.,()/]", "", anförande.avsnittsrubrik)
+        word_sum += clean + ' '  # full uncleaned string
 
     return word_sum
+
+def prepare(api, size):
+    full_text = {}
+    usage: Dict[riksdagen.Parti, Dict[str, float]] = {}
+    for p in riksdagen.Parti:
+        full_text = get_party_words(api, p, size)
+        words = [w.lower() for w in full_text.split(' ')]
+        usage[p] = {}
+        for word in words:
+            if word in usage[p]:
+                usage[p][word] += 1
+            else:
+                usage[p][word] = 1
+
+        for k, v in usage[p].items():
+            usage[p][k] = 100 * (v / len(words))
+
+    mean_usage: Dict[str, float] = {}
+    for p in riksdagen.Parti:
+        for k, v in usage[p].items():
+            if usage[p][k] in mean_usage:
+                mean_usage[k] += v / 8
+            else:
+                mean_usage[k] = v / 8
+
+    rel_usage: Dict[riksdagen.Parti, Dict[str, float]] = {}
+    for p in riksdagen.Parti:
+        rel_usage[p] = {}
+        for k, v in usage[p].items():
+            rel_usage[p][k] = v / mean_usage[k]
+
+    return rel_usage
 
 def print_party_common_words (relative_usage, length):
     sort = sorted(relative_usage, key=relative_usage.get, reverse=True)
     for position in range(length):
-        print(f'{position+1}. {sort[position]}, {relative_usage[sort[position]]:2f} % mer än snittet')
+        print(f'{position+1}. {sort[position]}, {relative_usage[sort[position]]:.0f} % mer än snittet')
 
-def test_data(parti):
-    text = {riksdagen.Parti.V:  "Ett Två Tre Tre",
-            riksdagen.Parti.C:  "Ett Två Tre Fyra",
-            riksdagen.Parti.KD: "Ett Två Tre",
-            riksdagen.Parti.M:  "Ett Två Tre",
-            riksdagen.Parti.L:  "Ett Två Tre",
-            riksdagen.Parti.SD: "Ett Två Tre",
-            riksdagen.Parti.MP: "Ett Två Tre",
-            riksdagen.Parti.S:  "Ett Två Tre"}
-    return text[p]
+def test_data():
+    text = ''
+    for i in range(300):
+        for j in range(random.randint(2, 9)):
+            text += random.choice(["a", "b", "c", "d", "e", "f"])
+        text += ' '
+    return text
+
+
+def create_fake_text(relative_usage: Dict[str, float], length):
+    sort = sorted(relative_usage, key=relative_usage.get, reverse=True)
+    big_string = ''
+    for position in range(length): # best to worst
+        copies = 1#int(relative_usage[sort[position]] * 10)
+        for cp in range(copies):
+            big_string += sort[position] + ' '
+
+    return big_string
+
+
+parti = riksdagen.Parti.SD
 
 api = riksdagen.API()
-
-full_text = {}
-#full_text[riksdagen.Parti.V] = get_party_words(api, riksdagen.Parti.V)
-usage: Dict[riksdagen.Parti, Dict[str, float]] = {}
-for p in riksdagen.Parti:
-    full_text = get_party_words(api, p)
-    #full_text = test_data(p)
-    words = [w.lower() for w in full_text.split(' ')]
-    usage[p] = {}
-    for word in words:
-        if word in usage[p]:
-            usage[p][word] += 1
-        else:
-            usage[p][word] = 1
-
-    for k, v in usage[p].items():
-        usage[p][k] = 100 * (v / len(words))
-
-mean_usage: Dict[str, float] = {}
-for p in riksdagen.Parti:
-    for k, v in usage[p].items():
-        if usage[p][k] in mean_usage:
-            mean_usage[k] += v / 8
-        else:
-            mean_usage[k] = v / 8
-
-relative_usage: Dict[riksdagen.Parti, Dict[str, float]] = {}
-for p in riksdagen.Parti:
-    relative_usage[p] = {}
-    for k, v in usage[p].items():
-        relative_usage[p][k] = v / mean_usage[k]
-
-print_party_common_words(relative_usage[riksdagen.Parti.SD], 15)
-
-#do_word_cloud(full_text)
-
-# do_word_cloud("The wealth of those societies in which the capitalist mode of production prevails, presents itself as "
-#              "an “immense accumulation of commodities”, its unit being a single commodity. "
-#              "(Translation by Samuel Moore and Edward Aveling, supervised by Friedrich Engels (Marx 1867/1965, 35))")
+relative_usage = prepare(api, 500)
+print_party_common_words(relative_usage[parti], 15)
+full_text = create_fake_text(relative_usage[parti], 50)
+do_cloud(full_text, parti)
